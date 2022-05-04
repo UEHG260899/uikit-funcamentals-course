73b02a8b61488a3da851882ec0fbf84c174283ea
Uriel Enrique <urielenrique99@gmail.com>
2022-05-04 18:38:47 -0500

    initial commit

---
+++ StoryPrompt/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Uriel Hernandez Gonzalez on 04/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let storyPrompt = StoryPromptEntry()
+        storyPrompt.noun = "toaster"
+        storyPrompt.adjective = "smelly"
+        storyPrompt.verb = "burps"
+        storyPrompt.number = 10
+        print(storyPrompt)
+    }
+
+
+}
+
