d1cca9861569555ce7028ed97775ddd9bfb6a316
Uriel Enrique <urielenrique99@gmail.com>
2022-05-04 20:00:28 -0500

    first course completed

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -6,19 +6,107 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextFIeld: UITextField!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var storyPromptImageView: UIImageView!
+    
+    let storyPrompt = StoryPromptEntry()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyPrompt = StoryPromptEntry()
-        storyPrompt.noun = "toaster"
-        storyPrompt.adjective = "smelly"
-        storyPrompt.verb = "burps"
-        storyPrompt.number = 10
-        print(storyPrompt)
+        numberSlider.value = 7.5
+        setupActions()
+    }
+    
+    private func setupActions() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+        storyPromptImageView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    private func changeImage() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 1
+        
+        let imageController = PHPickerViewController(configuration: configuration)
+        imageController.delegate = self
+        present(imageController, animated: true)
     }
 
-
+    
+    @IBAction func onNumberChanged(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+    
+    @IBAction func onStoryTypeChanged(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+    }
+    
+    @IBAction func onGenerateStoryClicked(_ sender: UIButton) {
+        updateStoryPrompt()
+        
+        if storyPrompt.isValid() {
+            print(storyPrompt)
+        } else {
+            let alert = UIAlertController(title: "Invalid Story Prompt", message: "Please fill out all of the fields", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            
+            alert.addAction(action)
+            present(alert, animated: true)
+        }
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        updateStoryPrompt()
+        return true
+    }
+    
+    func updateStoryPrompt() {
+        storyPrompt.noun = nounTextField.text ?? ""
+        storyPrompt.adjective = adjectiveTextField.text ?? ""
+        storyPrompt.verb = verbTextFIeld.text ?? ""
+    }
+}
+
+extension ViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        if !results.isEmpty {
+            let result = results.first!
+            let itemProvider = result.itemProvider
+            
+            if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, error in
+                    guard let image = image as? UIImage else {
+                        return
+                    }
+                    
+                    DispatchQueue.main.async {
+                        picker.dismiss(animated: true)
+                        self?.storyPromptImageView.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+}
